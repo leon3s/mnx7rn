9f5059c68c9746fe7bc314d5613af78ba05c86a6
leon3s <lvernisse@student.42.fr>
2022-03-27 19:32:27 +0200

    [WIPE] Regex to match host and username

--- src/cli/credential.ts
+++ src/cli/credential.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { homedir } from "os";
 import { readFile } from "fs/promises";
 
-export async function get_host_credential(host: string) {
+export async function get_host_credential(host: string, user?: string) {
   const home_dir = homedir();
   const content = await readFile(
     path.join(home_dir, '.nanocl-credentials'),
@@ -11,17 +11,11 @@ export async function get_host_credential(host: string) {
   if (!content || !content.length) {
     return null;
   }
-  content.split('\n')
-  .reduce((acc: null | Record<string, string>, line) => {
-    if (!line.includes(host)) {
-      return acc;
-    }
-    acc = {};
-    // const [credential] = line.
-    return acc;
-  }, null);
+  const regex = new RegExp(`(.*)\/\/(.*):(.*)@${host}`, 'g');
+  const s = "https://leon3s:super_password@github.com"
+  console.log(s.replace(regex, '$2:$3').split(':'));
 }
 
-export async function write_host_credential() {
+// export async function write_host_credential(host, credential) {
 
-}
+// }
