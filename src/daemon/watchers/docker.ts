2dff39ad974bb264c10e0345df5d91f54808a5bf
leon3s <lvernisse@student.42.fr>
2022-04-04 15:37:49 +0200

    [WIP] docker events in real time

--- src/daemon/watchers/docker.ts
+++ src/daemon/watchers/docker.ts
@@ -1,22 +1,10 @@
-import { endianness } from "os";
 import docker_api from "../api/docker";
 
 export async function watch_docker() {
-  const vres = await docker_api.system.version();
-  console.log(vres.data);
-  const en = endianness();
-  console.log(en);
-  const res = await docker_api.system.events();
-  res.socket.on('data', (buffer) => {
+  const {socket} = await docker_api.system.events();
+  socket.on('data', (buffer) => {
     const payload_size = parseInt(buffer.slice(0, 5).toString('utf-8'), 16);
     const json = buffer.subarray(5, payload_size + 5).toString('utf-8');
-    // const size = parseInt(code.toString('utf-8'), 16);
-    console.log({
-      payload_size,
-      length: json.length,
-    });
     console.log(JSON.parse(json));
-    // console.log(code.toString('utf-8'));
-    // console.log(JSON.parse(json));
   });
 }
